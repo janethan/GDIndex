ce438c083455a6513eeb454f3b7d155146495eba
janethan <lichuanjie@gmail.com>
2023-09-13 01:40:15 +0800

    Update worker1.js

--- worker1.js
+++ worker1.js
@@ -6,7 +6,7 @@ self.props = {
   "refresh_token": "",
   service_account: false, 
   service_account_json: {},
-  auth: false, //  默认关闭索引验证，改成`true`启用验证，启用之后用户名和密码才会生效
+  auth: false, //  默认关闭验证，改成`true`启用验证后用户名和密码才会生效
   "user": "", // 访问索引验证的用户名
   "pass": "", // 访问索引验证的密码
   upload: true, // 默认已开启上传功能，取消上传功能请改成`false`
